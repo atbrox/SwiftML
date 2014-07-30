08b1d705d0014f69f20739704c0cf0ea8e35b080
Amund Tveit <amund@memkite.com>
2014-07-30 14:15:28 +0200

    initial commit of xcode repo

---
+++ SwiftML/SwiftML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftML
+//
+//  Created by Amund Tveit on 30/07/14.
+//  Copyright (c) 2014 Amund Tveit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
